841c211f8de245eb13f7325de7d94e04fd2dcf02
Jacob Berman <jacobberman1995@gmail.com>
2018-04-01 21:01:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,14 @@ class Dog
   end
 
   def name 
-    puts @name
+     @name
   end 
   
   def breed=(breed)
     @breed = breed
   end 
   def breed 
-    puts @breed
+     @breed
   end 
   
 end 
